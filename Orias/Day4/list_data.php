848b4f455a4abb9c0dfdd930770656ea42e67b71
Orias1701 <long171.d.shine@gmail.com>
2025-09-03 08:31:47 +0700

    ..

--- Orias/Day4/list_data.php
+++ Orias/Day4/list_data.php
@@ -1,6 +1,34 @@
 <?php
 include 'config.php';
 
+// Thêm menu nút thao tác
+echo "
+<div style='margin-bottom:15px;'>
+    <form method='post'>
+        <button type='submit' name='action' value='create'>Tạo bảng</button>
+        <button type='submit' name='action' value='insert'>Thêm dữ liệu</button>
+        <button type='submit' name='action' value='update'>Cập nhật dữ liệu</button>
+        <button type='submit' name='action' value='delete'>Xóa dữ liệu</button>
+    </form>
+</div>
+";
+
+// Nếu nhấn nút thì chạy file tương ứng
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    if ($action == 'create') {
+        include 'create_table.php';
+    } elseif ($action == 'insert') {
+        include 'data_insert.php';
+    } elseif ($action == 'update') {
+        include 'data_update.php';
+    } elseif ($action == 'delete') {
+        include 'data_delete.php';
+    }
+    echo "<hr>";
+}
+
+// Hiển thị danh sách
 $sql = "SELECT id, firstname, lastname, reg_date FROM MyGuests";
 $result = $conn->query($sql);
 
@@ -10,7 +38,7 @@ echo "<table border='1' cellpadding='5'>
 <th>Id</th><th>Firstname</th><th>Lastname</th><th>Reg_Date</th>
 </tr>";
 
-if ($result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<tr>
             <td>".$row["id"]."</td>
